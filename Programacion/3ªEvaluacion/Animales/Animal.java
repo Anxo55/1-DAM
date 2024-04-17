921c5efdb93b0a6579ad0eb03ba1f28aac6af3e8
Anxo Campos Carballo <anxo.campos260204@gmail.com>
2024-04-17 12:49:36 +0200

    ubuntu

---
+++ Programacion/3ªEvaluacion/Animales/Animal.java
@@ -0,0 +1,26 @@
+package Animales;
+
+public abstract class Animal {
+
+  protected String sonido;
+  protected String alimentos;
+  protected String habitat;
+  protected String nombreCientifico;
+
+  
+  public String getSonido() {
+    return sonido;
+  }
+  public String getAlimentos() {
+    return alimentos;
+  }
+  public String getHabitat() {
+    return habitat;
+  }
+  public String getNombreCientifico() {
+    return nombreCientifico;
+  }
+  
+  
+    
+}
