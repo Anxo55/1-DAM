f5f387570ba2a34e42d6a41c481b8faf8f872a58
Anxo Campos Carballo <anxo.campos260204@gmail.com>
2024-04-24 14:48:56 +0200

    practica general

--- Programacion/3ªEvaluacion/CarreraCiclista/CIclista.java
+++ Programacion/3ªEvaluacion/CarreraCiclista/CIclista.java
@@ -1,43 +1,82 @@
 package CarreraCiclista;
 
-public abstract class CIclista {
+public abstract class Ciclista {
     
-    private int identificador;
-    private String nombre;
-    private int tiempoAcumulado = 0;
+    private int identificador; /* Atributo que define el identificador de un ciclista */
+	private String nombre; // Atributo que define el nombre del ciclista
+	private double tiempoAcumulado = 0; /* Atributo que define el tiempo acumulado de un ciclista */
+	private int posicionGeneral;
+
+	public Ciclista(int identificador, String nombre) {
+		this.identificador = identificador;
+		this.nombre = nombre;
+	}
     
-    public CIclista(int identificador, String nombre, int tiempoAcumulado) {
-        this.identificador = identificador;
-        this.nombre = nombre;
-        this.tiempoAcumulado = tiempoAcumulado;
-    }
 
-    abstract String imprimirTipo();
+	abstract String imprimirTipo();
 
-    public int getIdentificador() {
-        return identificador;
-    }
+	protected int getIdentificador() {
+		return identificador;
+	}
 
-    public void setIdentificador(int identificador) {
-        this.identificador = identificador;
-    }
+	protected void setIdentificador() {
+		this.identificador = identificador;
+	}
 
-    public String getNombre() {
-        return nombre;
-    }
+	protected String getNombre() {
+		return nombre;
+	}
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	protected void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-    public int getTiempoAcumulado() {
-        return tiempoAcumulado;
-    }
+	/**
+	 * Método que devuelve el puesto que ocupa un ciclista en la posición general de
+	 * la competencia
+	 * 
+	 * @return El puesto del ciclista en la posición general
+	 */
 
-    public void setTiempoAcumulado(int tiempoAcumulado) {
-        this.tiempoAcumulado = tiempoAcumulado;
-    }
+	protected int getPosicionGeneral(int posicionGeneral) {
+		return posicionGeneral;
+	}
 
-    
-    
+	/**
+	 * Método que establece el puesto que ocupa un ciclista en la posición general
+	 * 
+	 * @param Parámetro que especifica el puesto que ocupa un ciclista en la
+	 *                  posición general
+	 */
+	protected void setPosicionGeneral(int posicionGeneral) {
+		posicionGeneral = posicionGeneral;
+	}
+
+	/**
+	 * Método que devuelve el tiempo acumulado de un ciclista en una competencia
+	 * 
+	 * @return El tiempo acumulado de un ciclista en una competencia
+	 */
+	protected double getTiempoAcumulado() {
+		return tiempoAcumulado;
+	}
+
+	/**
+	 * Método que establece el tiempo acumulado por un ciclista
+	 * 
+	 * @param Parámetro que especifica el tiempo acumulado por un ciclista
+	 */
+	protected double setTiempoAcumulado(double tiempoAcumulado) {
+		return this.tiempoAcumulado = tiempoAcumulado;
+	}
+
+	/**
+	 * Método muestra en pantalla los datos de un ciclista
+	 */
+	protected void imprimir() {
+		System.out.println("Identificador = " + identificador);
+		System.out.println("Nombre = " + nombre);
+		System.out.println("Tiempo Acumulado = " + tiempoAcumulado);
+		System.out.println("Posicion = "+ posicionGeneral);
+	}
 }
--- Programacion/3ªEvaluacion/CarreraCiclista/Equipo.java
+++ Programacion/3ªEvaluacion/CarreraCiclista/Equipo.java
@@ -1,50 +1,102 @@
 package CarreraCiclista;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Equipo {
 
-    private String nombre;
-    private static double totalTiempo;
-    private String pais;
+    private String nombre; /* Atributo que define el nombre del equipo de ciclismo */
 
-    ArrayList<CIclista> listaCiclistas;
-    
-    public Equipo(String nombre, String pais) {
-        this.nombre = nombre;
-        this.pais = pais;
-        totalTiempo = 0;
-        listaCiclistas = new ArrayList<>();
-    }
+	private static double totalTiempo; /* Atributo que define el tiempo total obtenido por el equipo */
 
-    public String getNombre() {
-        return nombre;
-    }
+	private String pais; /* Atributo que define el país al que pertenece el equipo */
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	ArrayList<Ciclista> listaCiclistas; // Atributo que define un vector de objetos ciclista
 
-    public static double getTotalTiempo() {
-        return totalTiempo;
-    }
+	public Equipo(String nombre, String pais) {
+		this.nombre = nombre;
+		this.pais = pais;
+		totalTiempo = 0; // Se inicializa el tiempo del equipo en cero
+		listaCiclistas = new ArrayList<>(); /* Se crea el vector de ciclistas que conforma el equipo */
+	}
 
-    public static void setTotatTiempo(double totalTiempo) {
-        Equipo.totalTiempo = totalTiempo;
-    }
+	public String getNombre() {
+		return nombre;
+	}
 
-    public String getPais() {
-        return pais;
-    }
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-    public void setPais(String pais) {
-        this.pais = pais;
-    }
+	public String getPais() {
+		return pais;
+	}
 
-    void añadirCiclista(Ciclista ciclista) {
-        listaCiclistas.add(ciclista);
-    }
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
 
-    
-    
+	/**
+	 * Método que añade un ciclista al vector de ciclistas de un equipo
+	 */
+	void añadirCiclista(Ciclista ciclista) {
+		listaCiclistas.add(ciclista); // Se agrega el ciclista al vector de ciclistas
+	}
+
+	/**
+	 * Método que muestra en pantalla los nombres de los ciclistas que conforman un
+	 * equipo
+	 */
+	void listarEquipo() {
+		/*
+		 * Se recorre el vector de ciclistas y para cada elemento se imprime el nombre
+		 * del ciclista
+		 */
+		for (int i = 0; i < listaCiclistas.size(); i++) {
+			Ciclista c = (Ciclista) listaCiclistas.get(i); /* Se aplica casting para extraer el elemento */
+			System.out.println(c.getNombre());
+		}
+	}
+
+	/**
+	 * Método que busca un ciclista ingresado por teclado
+	 */
+	void buscarCiclista() {
+		Scanner sc = new Scanner(System.in); /* Se solicita texto ingresado por teclado */
+		System.out.println("Dame nombre: ");
+		boolean esta = false;
+		String nombreCiclista = sc.nextLine();
+		for (int i = 0; i < listaCiclistas.size(); i++) { /* Se recorre el vector de ciclistas */
+			Ciclista c = (Ciclista) listaCiclistas.get(i); /* Se obtiene un elemento del vector */
+			if (c.getNombre().equals(nombreCiclista)) { /* Si el nombre ingresado se encuentra */
+				System.out.println(c.getNombre());
+				esta = true;
+			}
+			}
+		if(!esta) {
+			System.out.println("No encontrado");
+		}
+		}
+	
+
+	/**
+	 * Método que calcula el tiempo total de un equipo acumulando el tiempo obtenido
+	 * por cada uno de sus ciclistas
+	 */
+	void calcularTotalTiempo() {
+		for (int i = 0; i < listaCiclistas.size(); i++) { // Se recorre el vector
+			Ciclista c = (Ciclista) listaCiclistas.get(i); /* Se obtiene un elemento del vector */
+			// Se acumula el tiempo del ciclista en el tiempo del equipo
+			totalTiempo += c.getTiempoAcumulado();
+		}
+	}
+
+	/**
+	 * Método que muestra en pantalla los datos de un equipo
+	 */
+	void imprimir() {
+		System.out.println("Nombre del equipo = " + nombre);
+		System.out.println("Pais = " + pais);
+		System.out.println("Total tiempo del equipo = " + totalTiempo);
+	}
 }
